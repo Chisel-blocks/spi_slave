b35b740ababca09334d00b8939637011362842ff
Ilia Kempi <ilia.kempi@aalto.fi>
2019-09-09 12:17:01 +0300

    falling edge

--- src/main/scala/spi_slave/spi_slave.scala
+++ src/main/scala/spi_slave/spi_slave.scala
@@ -1,56 +1,63 @@
-
-// Dsp-block spi_slave
-// Description here
-// Inititally written by dsp-blocks initmodule.sh, 20190905
 package spi_slave
 
-//import chisel3.experimental._
 import chisel3._
 import chisel3.iotesters.{PeekPokeTester, Driver}
-//import dsptools.{DspTester, DspTesterOptionsManager, DspTesterOptions}
-//import dsptools.numbers._
-//import breeze.math.Complex
+import chisel3.experimental.{withClock}
 
-//class spi_slave_io[T <:Data](proto: T,n: Int)
-//   extends Bundle {
-//        val A       = Input(Vec(n,proto))
-//        val B       = Output(Vec(n,proto))
-//        override def cloneType = (new spi_slave_io(proto.cloneType,n)).asInstanceOf[this.type]
-//   }
-
-class spi_slave extends Module {
+class spi_slave(val cfg_length : Int = 8) extends Module {
     val io = IO(new Bundle{
       val mosi = Input(UInt(1.W))
       val cs   = Input(UInt(1.W))
       val miso = Output(UInt(1.W))
+      val inv_sclk = Input(Clock())
+      val config_out = Output(UInt(cfg_length.W))
     })
-    io.miso:= io.mosi & io.cs
+
+    val spi_enabled = !io.cs.toBool
+
+    withClock(io.inv_sclk){
+      val state = Reg(UInt(cfg_length.W))
+      val nextState = (state << 1) | io.mosi
+      when (spi_enabled) {
+          state := nextState
+      }
+    }
+
+    io.config_out := state
+    when (spi_enabled) { 
+      io.miso := state(cfg_length-1)
+    }
+    .otherwise {
+      io.miso := 0.U
+    }
+
 }
 
-//This gives you verilog
 object spi_slave extends App {
     chisel3.Driver.execute(args, () => new spi_slave)
-
-    //chisel3.Driver.execute(args, () => new spi_slave(
-    //    proto=DspComplex(UInt(16.W),UInt(16.W)), n=8)
-    //)
 }
 
-//This is a simple unit tester for demonstration purposes
-class unit_tester(c: spi_slave ) extends PeekPokeTester(c) {
-//Tests are here 
-    poke(c.io.mosi, 1)
-    poke(c.io.cs, 0)
-    step(1)
-    expect(c.io.miso,0)
-
+class unit_tester(c: spi_slave) extends PeekPokeTester(c) {
+  
+    // tests that the mosi passes through the register not sooner than full cycle
     poke(c.io.mosi, 1)
     poke(c.io.cs, 1)
+    for (i <- 0 to 2){
+      step(1)
+      expect(c.io.miso, 0)
+    }
+
+    poke(c.io.cs, 0)
+    for (i <- 1 to 7){
+      step(1)
+      expect(c.io.miso, 0)
+    }
     step(1)
-    expect(c.io.miso,1)
+    expect(c.io.miso, 1)
+    step(1)
+    expect(c.io.miso, 1)
 }
 
-//This is the test driver 
 object unit_test extends App {
     iotesters.Driver.execute(args, () => new spi_slave){
             c=>new unit_tester(c)
