e28d105e0aff286058032bcdf77ddd348bab0954
Marko Kosunen <marko.kosunen@aalto.fi>
2019-09-10 18:00:14 +0300

    Add parameters

--- src/main/scala/spi_slave/spi_slave.scala
+++ src/main/scala/spi_slave/spi_slave.scala
@@ -18,9 +18,10 @@ class spi_slave(val cfg_length : Int = 8, val monitor_length : Int =8) extends M
 
     val serial_register = withClock(inv_sclk){Reg(Vec(io.config_out.getWidth+io.monitor_in.getWidth,UInt(1.W)))}
     val config_out_reg =  Reg(Vec(cfg_length,UInt(1.W)))
-    val monitor_in_reg =  Reg(Vec(cfg_length,UInt(1.W)))
+    val monitor_in_reg =  Reg(Vec(monitor_length,UInt(1.W)))
         
     // Check the endianness
+    //Requires condition
     when ( spi_enabled === 1.U) {
         serial_register(0):=io.mosi
         for ( i <- 1 until serial_register.length ) {
@@ -30,9 +31,8 @@ class spi_slave(val cfg_length : Int = 8, val monitor_length : Int =8) extends M
         for ( i <- 0 until config_out_reg.getWidth ) {
             config_out_reg(i) := serial_register(i)
         }
-
-        //Requires condition
         for ( i <- 0 until monitor_in_reg.getWidth ) {
+            //println(i)
             serial_register(config_out_reg.getWidth+i) := monitor_in_reg(i).asUInt()
         }
     }
@@ -51,8 +51,63 @@ class spi_slave(val cfg_length : Int = 8, val monitor_length : Int =8) extends M
 
 }
 
+
+
+
+//This is the object to provide verilog
 object spi_slave extends App {
-    chisel3.Driver.execute(args, () => new spi_slave)
+    // Getopts parses the "Command line arguments for you"  
+    def getopts(options : Map[String,String], 
+        arguments: List[String]) : (Map[String,String], List[String]) = {
+        //This the help
+        val usage = """
+            |Usage: spi_slave.spi_slave [-<option>]
+            |
+            | Options
+            |     cfg_length       [Int]     : Number of bits in the config register. Default 8
+            |     monitor_length   [Int]     : Number of bits in the monitor register. Default 8
+            |     h                       : This help 
+          """.stripMargin
+        val optsWithArg: List[String]=List(
+            "-cfg_length",
+            "-monitor_length"
+        )
+        //Handling of flag-like options to be defined 
+        arguments match {
+            case "-h" :: tail => {
+                println(usage)
+                val (newopts, newargs) = getopts(options, tail)
+                sys.exit
+                (Map("h"->"") ++ newopts, newargs)
+            }
+            case option :: value :: tail if optsWithArg contains option => {
+               val (newopts, newargs) = getopts(
+                   options++Map(option.replace("-","") -> value), tail
+               )
+               (newopts, newargs)
+            }
+              case argument :: tail => {
+                 val (newopts, newargs) = getopts(options,tail)
+                 (newopts, argument.toString +: newargs)
+              }
+            case Nil => (options, arguments)
+        }
+    }
+     
+    // Default options
+    val defaultoptions : Map[String,String]=Map(
+        "cfg_length"->"8",
+        "monitor_length"->"8"
+        ) 
+    // Parse the options
+    val (options,arguments)= getopts(defaultoptions,args.toList)
+  
+    chisel3.Driver.execute(arguments.toArray, () => 
+            new spi_slave(
+                cfg_length=options("cfg_length").toInt, 
+                monitor_length=options("monitor_length").toInt
+            )
+    )
 }
 
 class unit_tester(c: spi_slave) extends PeekPokeTester(c) {
